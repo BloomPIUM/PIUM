08293d210ac210d7c1befd20c0dfea883fd65ca2
99cp <103494820+99cp@users.noreply.github.com>
2023-09-20 21:17:27 +0900

    제발요

---
+++ src/main/java/com/bloom/pium/controller/BoardController.java
@@ -0,0 +1,38 @@
+package com.bloom.pium.controller;
+
+import com.bloom.pium.data.dto.BoardDto;
+import com.bloom.pium.data.dto.BoardResponseDto;
+import com.bloom.pium.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    private final BoardService boardService;
+
+    @Autowired
+    public BoardController(BoardService boardService){
+        this.boardService = boardService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<BoardResponseDto> getBoard(Long boardId){
+        BoardResponseDto boardResponseDto = boardService.getBoard(boardId);
+        return ResponseEntity.status(HttpStatus.OK).body(boardResponseDto);
+    }
+
+    @GetMapping("/form")
+    public String goToBoard() {
+        return "BoardForm";
+    }
+    @PostMapping("write")
+    public  ResponseEntity<BoardResponseDto> createdBoard(@RequestBody BoardDto boardDto){
+        BoardResponseDto boardResponseDto = boardService.saveBoard(boardDto);
+        return ResponseEntity.status(HttpStatus.OK).body(boardResponseDto);
+    }
+
+}
